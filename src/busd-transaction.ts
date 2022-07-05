a650d7e37c0a87b9b89cbd96e26a986677a00487
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-07-04 21:56:52 -0300

    refactoring DepositTransaction and WithdrawalTransaction into a single BUSDTransaction entity, and making it immutable

--- src/busd-transaction.ts
+++ src/busd-transaction.ts
@@ -1,10 +1,6 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts";
 import { Transfer } from "../generated/BEP20Token/BEP20Token";
-import {
-  DepositTransaction,
-  Player,
-  WithdrawalTransaction,
-} from "../generated/schema";
+import { BUSDTransaction, Player } from "../generated/schema";
 import * as player from "./player";
 
 export function handleTransfer(event: Transfer): void {
@@ -13,32 +9,38 @@ export function handleTransfer(event: Transfer): void {
     "0x00574b151CB66D51b444597c6a4BbF1a44eE5fe9", // dev
   ];
   let p: Player;
+  let transaction: BUSDTransaction;
+  let isGameTransaction = false;
   let isWithdrawal = false;
 
   for (let i = 0; i < bucketWalletAddresses.length; i++) {
     if (
       Address.fromHexString(bucketWalletAddresses[i]).equals(event.params.from)
     ) {
+      isGameTransaction = true;
       isWithdrawal = true;
+      break;
+    } else if (
+      Address.fromHexString(bucketWalletAddresses[i]).equals(event.params.to)
+    ) {
+      isGameTransaction = true;
       break;
     }
   }
 
-  if (isWithdrawal) {
-    // Withdrawal Transaction
-    p = player.loadOrCreate(event.params.to);
-    const transaction = new WithdrawalTransaction(
-      event.transaction.hash.toHex()
-    );
-    transaction.player = p.id;
-    transaction.value = event.params.value;
-    transaction.save();
-  } else {
-    // Deposit Transaction
-    p = player.loadOrCreate(event.params.from);
-    const transaction = new DepositTransaction(event.transaction.hash.toHex());
-    transaction.player = p.id;
-    transaction.value = event.params.value;
-    transaction.save();
+  if (!isGameTransaction) {
+    return;
   }
+
+  if (isWithdrawal) {
+    p = player.loadOrCreate(event.params.to);
+  } else {
+    p = player.loadOrCreate(event.params.from);
+  }
+
+  transaction = new BUSDTransaction(event.transaction.hash.toHex());
+  transaction.player = p.id;
+  transaction.value = event.params.value;
+  transaction.isWithdrawal = isWithdrawal;
+  transaction.save();
 }
