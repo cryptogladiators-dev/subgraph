6d77e6991ef6e533e393326837a1ec223bcceab3
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-07-06 19:05:27 -0300

    refactor: BUSD_WALLET_ADDRESSES from constants.ts

--- src/busd-transaction.ts
+++ src/busd-transaction.ts
@@ -1,27 +1,25 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Address } from "@graphprotocol/graph-ts";
 import { Transfer } from "../generated/BEP20Token/BEP20Token";
 import { BUSDTransaction, Player } from "../generated/schema";
+import { BUCKET_WALLET_ADDRESSES } from "./constants";
 import * as player from "./player";
 
 export function handleTransfer(event: Transfer): void {
-  const bucketWalletAddresses: string[] = [
-    "0xcc21Bc565fED141A434b1730885efbc692A130F9", // localhost
-    "0x00574b151CB66D51b444597c6a4BbF1a44eE5fe9", // dev
-  ];
   let p: Player;
-  let transaction: BUSDTransaction;
   let isGameTransaction = false;
   let isWithdrawal = false;
 
-  for (let i = 0; i < bucketWalletAddresses.length; i++) {
+  for (let i = 0; i < BUCKET_WALLET_ADDRESSES.length; i++) {
     if (
-      Address.fromHexString(bucketWalletAddresses[i]).equals(event.params.from)
+      Address.fromHexString(BUCKET_WALLET_ADDRESSES[i]).equals(
+        event.params.from
+      )
     ) {
       isGameTransaction = true;
       isWithdrawal = true;
       break;
     } else if (
-      Address.fromHexString(bucketWalletAddresses[i]).equals(event.params.to)
+      Address.fromHexString(BUCKET_WALLET_ADDRESSES[i]).equals(event.params.to)
     ) {
       isGameTransaction = true;
       break;
@@ -38,7 +36,7 @@ export function handleTransfer(event: Transfer): void {
     p = player.loadOrCreate(event.params.from);
   }
 
-  transaction = new BUSDTransaction(event.transaction.hash.toHex());
+  const transaction = new BUSDTransaction(event.transaction.hash.toHex());
   transaction.player = p.id;
   transaction.value = event.params.value;
   transaction.isWithdrawal = isWithdrawal;
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+const BUCKET_WALLET_ADDRESSES: string[] = [
+  "0xcc21Bc565fED141A434b1730885efbc692A130F9", // localhost
+  "0x00574b151CB66D51b444597c6a4BbF1a44eE5fe9", // dev
+];
+
+export { BUCKET_WALLET_ADDRESSES };
