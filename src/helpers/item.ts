8275124980369551103575ea9382a8137596bfe2
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-07-10 03:10:43 -0300

    updating WalletOwnedItem on Item transfer

--- src/helpers/item.ts
+++ src/helpers/item.ts
@@ -1,27 +1,64 @@
-import { BigInt, log } from "@graphprotocol/graph-ts";
-import { Item } from "../../generated/schema";
+import {
+  BigInt,
+  Address,
+  log,
+  crypto,
+  ByteArray,
+} from "@graphprotocol/graph-ts";
+import { Item, Wallet, WalletOwnedItem } from "../../generated/schema";
+import { loadOrCreateWallet } from "./wallet";
 
-function idFromBigInt(id: BigInt): string {
+function createItemID(id: BigInt): string {
   return id
     .toHexString()
     .slice(2)
     .padStart(64, "0");
 }
 
+function createWalletOwnedItemID(wallet: Wallet, item: Item): string {
+  const id = ByteArray.fromHexString(wallet.id).concat(
+    ByteArray.fromHexString(item.id)
+  );
+  return crypto.keccak256(id).toHexString();
+}
+
 export function loadItem(id: BigInt): Item | null {
-  const idHex = idFromBigInt(id);
+  const idHex = createItemID(id);
   return Item.load(idHex);
 }
 
 export function createItem(id: BigInt, amount: BigInt): Item {
-  const idHex = idFromBigInt(id);
+  const idHex = createItemID(id);
   const item = new Item(idHex);
   item.total = item.inStock = amount;
   item.save();
   return item;
 }
 
-export function mint(id: BigInt, amount: BigInt): Item {
+export function updateWalletOwnedItem(
+  wallet: Wallet,
+  item: Item,
+  amount: BigInt,
+  add: boolean = true
+): WalletOwnedItem {
+  const id = createWalletOwnedItemID(wallet, item);
+  let walletOwnedItem = WalletOwnedItem.load(id);
+  if (!walletOwnedItem) {
+    walletOwnedItem = new WalletOwnedItem(id);
+    walletOwnedItem.wallet = wallet.id;
+    walletOwnedItem.item = item.id;
+    walletOwnedItem.amount = BigInt.zero();
+  }
+  if (add) {
+    walletOwnedItem.amount = walletOwnedItem.amount.plus(amount);
+  } else {
+    walletOwnedItem.amount = walletOwnedItem.amount.minus(amount);
+  }
+  walletOwnedItem.save();
+  return walletOwnedItem;
+}
+
+export function mint(id: BigInt, amount: BigInt, to: Address): Item {
   log.info("Handling mint of {} {} with id {}", [
     amount.toString(),
     amount.equals(BigInt.fromI32(1)) ? "Item" : "Items",
@@ -34,13 +71,19 @@ export function mint(id: BigInt, amount: BigInt): Item {
   item.total = item.total.plus(amount);
   item.inStock = item.inStock.plus(amount);
   item.save();
+  const wallet = loadOrCreateWallet(to);
+  updateWalletOwnedItem(wallet, item, amount);
   return item;
 }
 
-export function mintBatch(ids: BigInt[], amounts: BigInt[]): Item[] {
+export function mintBatch(
+  ids: BigInt[],
+  amounts: BigInt[],
+  to: Address
+): Item[] {
   const items: Item[] = [];
   for (let i = 0; i < ids.length; i++) {
-    const item = mint(ids[i], amounts[i]);
+    const item = mint(ids[i], amounts[i], to);
     items.push(item);
   }
   return items;
--- src/item-transaction.ts
+++ src/item-transaction.ts
@@ -2,42 +2,52 @@ import {
   TransferBatch,
   TransferSingle,
 } from "../generated/CryptoGladiatorsItems/CryptoGladiatorsItems";
-import { ItemTransaction, Wallet } from "../generated/schema";
+import { Item, ItemTransaction, Wallet } from "../generated/schema";
 import { loadOrCreateWallet } from "./helpers/wallet";
-import { loadItem, mint, mintBatch } from "./helpers/item";
+import {
+  loadItem,
+  mint,
+  mintBatch,
+  updateWalletOwnedItem,
+} from "./helpers/item";
 import { Address } from "@graphprotocol/graph-ts";
 
 export function handleTransferSingle(event: TransferSingle): void {
   if (event.params.from.equals(Address.zero())) {
-    mint(event.params.id, event.params.value);
+    mint(event.params.id, event.params.value, event.params.to);
     return;
   }
 
-  const itemId = loadItem(event.params.id)!.id;
+  const item = loadItem(event.params.id)!;
   const from: Wallet = loadOrCreateWallet(event.params.from);
   const to: Wallet = loadOrCreateWallet(event.params.to);
 
   const transaction = new ItemTransaction(event.transaction.hash.toHex());
   transaction.from = from.id;
   transaction.to = to.id;
-  transaction.items = [itemId];
+  transaction.items = [item.id];
   transaction.values = [event.params.value];
   transaction.type = "SINGLE";
   transaction.createdAt = event.block.timestamp;
   transaction.blockNumber = event.block.number.toI32();
   transaction.save();
+
+  updateWalletOwnedItem(from, item, event.params.value, false);
+  updateWalletOwnedItem(to, item, event.params.value, true);
 }
 
 export function handleTransferBatch(event: TransferBatch): void {
   if (event.params.from.equals(Address.zero())) {
-    mintBatch(event.params.ids, event.params.values);
+    mintBatch(event.params.ids, event.params.values, event.params.to);
     return;
   }
 
+  const items: Item[] = [];
   const itemIds: string[] = [];
   for (let i = 0; i < event.params.ids.length; i++) {
-    const itemId = loadItem(event.params.ids[i])!.id;
-    itemIds.push(itemId);
+    const item: Item = loadItem(event.params.ids[i])!;
+    items.push(item);
+    itemIds.push(item.id);
   }
   const from: Wallet = loadOrCreateWallet(event.params.from);
   const to: Wallet = loadOrCreateWallet(event.params.to);
@@ -51,4 +61,9 @@ export function handleTransferBatch(event: TransferBatch): void {
   transaction.createdAt = event.block.timestamp;
   transaction.blockNumber = event.block.number.toI32();
   transaction.save();
+
+  for (let i = 0; i < items.length; i++) {
+    updateWalletOwnedItem(from, items[i], event.params.values[i], false);
+    updateWalletOwnedItem(to, items[i], event.params.values[i], true);
+  }
 }
